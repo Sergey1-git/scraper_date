8d27d078e70fe83b398665ac701ef6c3536b1195
Sergey1-git <Sergey1-git.yan.dex.ru@yandex.ru>
2025-09-20 14:03:47 +0300

    add in file date.csv

--- grafhics.py
+++ grafhics.py
@@ -6,13 +6,13 @@ from matplotlib.ticker import IndexLocator
 df = pd.read_csv('date.csv')
 df['Datetime'] = pd.to_datetime(df['Datetime'], format='%Y-%m-%d %H:%M')
 t=df[df['Name'].isin(['LKOH'])]
-print(t)
 
 fig, ax = plt.subplots()
 ax.plot(t['Datetime'], t['Price'])
 ax.xaxis.set_major_locator(mdates.AutoDateLocator())
+ax.yaxis.set_major_locator(mdates.AutoDateLocator())
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M'))
-ax.yaxis.set_major_locator(IndexLocator(base=10, offset=0))
+#ax.yaxis.set_major_locator(IndexLocator(base=2, offset=0))
 ax.set_title("График изменения цены акции LKOH")
 ax.set_xlabel('Datetime')
 ax.set_ylabel('Price')
